395f65405b380e0de9b9ff34af7f6777266b57ca
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-10-04 20:18:58 +0200

    changed name of class from pts to htmx

--- src/main/java/dev/naimsulejmani/pts/HtmxApplication.java
+++ src/main/java/dev/naimsulejmani/pts/HtmxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PtsApplication {
+public class HtmxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PtsApplication.class, args);
+        SpringApplication.run(HtmxApplication.class, args);
     }
 
 }
--- src/test/java/dev/naimsulejmani/pts/HtmxApplicationTests.java
+++ src/test/java/dev/naimsulejmani/pts/HtmxApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PtsApplicationTests {
+class HtmxApplicationTests {
 
     @Test
     void contextLoads() {
